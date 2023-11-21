4fe5f5adb43ed1a962128bdd4ab6db7d0bf4d628
jose <jose.dubon@kattangroup.com>
2023-11-20 20:10:12 -0600

    Agregar archivos de proyecto.

---
+++ invertit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Ingrese una cadena de texto: ");
+        string input = Console.ReadLine();
+
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        string invertirTexto = new string(charArray);
+
+        Console.WriteLine("Cadena invertida: " + invertirTexto);
+
+        Console.ReadLine();
+
+
+    }
+}
